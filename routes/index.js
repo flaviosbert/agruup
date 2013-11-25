ab207204b28d1dc40bb6d2ecbe77401f83dd4c84
Flavio Bertaggia <flaviobertaggia@gmail.com>
2013-11-25 19:36:27 -0300

    correções para funcionar com express-myconnection

--- routes/index.js
+++ routes/index.js
@@ -42,25 +42,30 @@ exports.concursos = {};
 
 exports.concursos.all = function(req, res) {
 	var stringSQL = "select * from concursos;";
-	db.query(stringSQL, function(err, rows) {
-		if (err) {
-			throw err;
-		}
-		
-		if (!rows) {
-			res.json("{}");
-		}
-		
-		var ret = [];
-		
-		for (var i=0; i<rows.length; i++) {
-			var row = rows[i];
+	
+	req.getConnection(function (err, connection){
+		if (err) return next(err);
+
+		connection.query(stringSQL, function(err, rows) {
+			if (err) {
+				throw err;
+			}
 			
-			var linha = concursoToObj(row);			
-			ret.push(linha);			
-		}		
-		
-		res.json(ret);
+			if (!rows) {
+				res.json("{}");
+			}
+			
+			var ret = [];
+			
+			for (var i=0; i<rows.length; i++) {
+				var row = rows[i];
+				
+				var linha = concursoToObj(row);			
+				ret.push(linha);			
+			}		
+			
+			res.json(ret);
+		});
 	});
 	
 //	
@@ -74,13 +79,18 @@ exports.concursos.one = function(req, res) {
 	var id = req.params.id;
 	var stringSQL = "select * from concursos where idconcurso=?";
 	
-	db.query(stringSQL, id, function(err, rows) {
-		if (err) throw err;		
-		if (!rows) throw new Error("Concurso nao encontrado"); 
-		
-		var obj = concursoToObj(rows[0]);
-		
-		res.json(obj);
+	
+	req.getConnection(function (err, connection){
+		if (err) return next(err);
+
+		connection.query(stringSQL, id, function(err, rows) {
+			if (err) throw err;		
+			if (!rows) throw new Error("Concurso nao encontrado"); 
+			
+			var obj = concursoToObj(rows[0]);
+			
+			res.json(obj);
+		});
 	});
 	
 //	db.concursos.find({_id: id}, function(err, obj) {
@@ -98,11 +108,15 @@ exports.concursos.create = function(req, res) {
 	concurso.salario = req.body.salario;
 	
 	var stringSQL = "INSERT INTO concursos SET ?";
-	var query = db.query(stringSQL, concurso, function(err, result) {
-		if (err) throw err;
-		
-		concurso.id = result.insertId;
-		res.json(concurso);		
+	req.getConnection(function (err, connection){
+		if (err) return next(err);
+
+		connection.query(stringSQL, concurso, function(err, result) {
+			if (err) throw err;
+			
+			concurso.id = result.insertId;
+			res.json(concurso);		
+		});
 	});
 	
 //	db.concursos.save(concurso, function(err, obj) {
@@ -118,10 +132,14 @@ exports.concursos.change = function(req, res) {
 	delete concurso.id;
 	
 	var stringSQL = "UPDATE concursos SET ? WHERE IDCONCURSO=?";
-	var query = db.query(stringSQL, [concurso, id], function(err, result) {
-		if (err) throw err;
-		
-		res.json(req.body);
+	req.getConnection(function (err, connection){
+		if (err) return next(err);
+
+		connection.query(stringSQL, [concurso, id], function(err, result) {
+			if (err) throw err;
+			
+			res.json(req.body);
+		});
 	});
 	
 //	db.concursos.update({"_id": id }, concurso, function(err, obj) {
@@ -134,11 +152,14 @@ exports.concursos.change = function(req, res) {
 exports.concursos.remove = function(req, res) {
 	var id = req.params.id;
 	var stringSQL = "DELETE FROM concursos WHERE idconcurso=?";
-	db.query(stringSQL, id, function(err, result) {
-		if (err) throw err;
-		res.json('{ok:true}');		
+	req.getConnection(function (err, connection){
+		if (err) return next(err);
+
+		connection.query(stringSQL, id, function(err, result) {
+			if (err) throw err;
+			res.json('{ok:true}');		
+		});
 	});
-	
 //	db.concursos.remove({"_id": id }, function(err, obj) {
 //		if (err) throw err;
 //		res.json('{ok:true}');
